59454af8f8269f9343e5a653da353e6e8dcf4be6
Sarath-Gupta <98757126+Sarath-Gupta@users.noreply.github.com>
2023-06-21 22:26:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 var bird;
+var pipes = [];
 function setup() {
   createCanvas(400, 600);
   bird = new Bird();
+  pipes.push(new Pipe());
   
 }
 
@@ -9,6 +11,15 @@ function draw() {
   background(0);
   bird.show();
   bird.update();
+  
+  if (frameCount % 100 == 0) {
+    pipes.push(new Pipe());
+  }
+  
+  for(var i = 0; i < pipes.length; i++) {
+    pipes[i].show();
+    pipes[i].update();
+  }
 }
 
 function mouseClicked() {
