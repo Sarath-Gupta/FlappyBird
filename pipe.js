dab5150370a0112c874e63abe0f5934520c4217f
Sarath-Gupta <98757126+Sarath-Gupta@users.noreply.github.com>
2023-06-28 23:48:53 +0530

    Update pipe.js

--- pipe.js
+++ pipe.js
@@ -4,13 +4,43 @@ function Pipe(){
   this.x = width;
   this.w = 20;
   this.speed = 2;
+  this.hightlight = false;
+  this.passed = false;
+  
+  
+  this.hits = function() {
+    if (bird.y < this.top || bird.y > height - this.bottom ) {
+      if(bird.x > this.x && bird.x < this.x + this.w) {
+        this.hightlight = true;
+        return true;
+      }
+    }
+    this.highlight = false;
+    return false;
+  }
   
   this.show = function(){
     fill(255);
+    if (this.hightlight) {
+      fill(255,0,0);
+    }
     rect(this.x,0,this.w,this.top);
     rect(this.x,height-this.bottom,this.w,this.bottom);
   }
+  
   this.update = function(){
-    this.x = this.x - this.speed;  
+    this.x = this.x - this.speed;
+    if (this.x < -this.w && !this.passed) {
+    this.passed = true;
+    }
+  }
+  
+  this.offscreen = function() {
+    if (this.x < -this.w) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 }
